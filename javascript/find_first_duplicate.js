bf7dc69339b9c48e6ff18112bd40e4ce1da7357a
Tasreen Mowreen <tmowreen97@gmail.com>
2022-04-30 15:59:20 -0400

    commit tree aace538cde1fc13c92a9e6f7d8ef5ed9f020a555

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,20 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let elementSet = new Set();
+  for (let i=0; i<arr.length; i++){
+    if (elementSet.has(arr[i])) return arr[i];
+    elementSet.add(arr[i]);
+
+  }
+  return -1
+  
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 1");
+  console.log("=>", findFirstDuplicate([4,1,1,2,3,4,5]));
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -17,3 +28,4 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// we need to first select one value in the Array, then search the rest of the array for that duplicate value. This would take more and more time depending on how long the array is. This asks for the first duplicate value. 
